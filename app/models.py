624b2ac003b8ccd2f1bacdacf8d45ce871eb8b2d
bashikr <bashikr@gmail.com>
2021-11-30 16:15:28 +0100

    fix: add docstrings to app/models.py

--- app/models.py
+++ app/models.py
@@ -48,18 +48,30 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def follow(self, user):
+        """
+        Follow a user
+        """
         if not self.is_following(user):
             self.followed.append(user)
 
     def unfollow(self, user):
+        """
+        Unfollow a user
+        """
         if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
+        """
+        Check if the user who is about to be followed is already followed
+        """
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
+        """
+        Brings the followed user's posts into the following users timeline
+        """
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
